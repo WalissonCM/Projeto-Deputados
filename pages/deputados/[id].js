327401ed2527d89464f292429ab319e54348da0a
WalissonCM <walissoncavalcante@outlook.com>
2023-05-03 21:23:49 -0300

    projeto

--- pages/deputados/[id].js
+++ pages/deputados/[id].js
@@ -3,65 +3,61 @@ import apiDeputados from '@/services/apiDeputados'
 import React from 'react'
 import { Col, Row, Card, Button, Table } from 'react-bootstrap'
 
-const Detalhes = ({deputados, despesas, profissoes}) => {
+const Detalhes = ({ deputados, despesas, profissoes }) => {
   return (
-      <Pagina titulo="Deputados">
-       
-       <Row>
-          <Col md={3}>
+    <Pagina titulo="Deputados">
+
+      <Row>
+        <Col md={3}>
           <Card style={{ width: '18rem' }}>
             <Card.Img src={deputados.ultimoStatus.urlFoto} width="100px" variant="top" />
-          <Card.Body>
-            <Card.Title>{deputados.nomeCivil}</Card.Title>
+            <Card.Body>
+              <Card.Title>{deputados.nomeCivil}</Card.Title>
               <Card.Text>
-               Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+                <p>Partido: {deputados.ultimoStatus.siglaPartido}</p>
+                <p>UF: {deputados.ultimoStatus.siglaUf}</p>
               </Card.Text>
-           </Card.Body>  
+            </Card.Body>
           </Card>
           <br />
-          <Button variant="dark">vote</Button>
-          </Col>
-        
-          <Col md={3}>
-            <h2>Despesas</h2>
-            
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                      <th>Data</th>
-                      <th>Descrição</th>
-                      <th>Valor</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                      <td>1</td>
-                       <td> {despesas.ano.map(item => (<p>{item.name}</p>))}</td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                       <td> {despesas.tipoDespesa.map(item => (<p>{item.name}</p>))}</td>
-                    </tr>
-                    <tr>
-                      <td>3</td>
-                       <td> {despesas.valorLiquido.map(item => (<p>{item.name}</p>))}</td>
-                    </tr>
-                </tbody>
-            </Table>   
-          </Col>
+          <Button href="/deputados" variant="dark">voltar</Button>
+        </Col>
 
-          <Col md={3}>
-            <h2>Profissões</h2>
-          
-                {profissoes.map(item => (
-                  <p>{item.name}</p>
-                ))}
-          </Col>
-           
-        </Row>
-      
-      </Pagina>
+        <Col md={6}>
+          <h2>Despesas</h2>
+
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Data</th>
+                <th>Descrição</th>
+                <th>Valor</th>
+              </tr>
+            </thead>
+            <tbody>
+              {despesas.map(item => (
+                <tr>
+                  <td>{item.dataDocumento}</td>
+                  <td>{item.tipoDespesa}</td>
+                  <td>{item.valorDocumento}</td>
+                </tr>
+                  ))}
+            </tbody>
+          </Table>
+        </Col>
+
+        <Col md={3}>
+          <h2>Profissões</h2>
+          <ul>
+            {profissoes.map(item => (
+              <li>{item.titulo}</li>
+            ))}
+          </ul>
+        </Col>
+
+      </Row>
+
+    </Pagina>
   )
 }
 
@@ -69,18 +65,18 @@ export default Detalhes
 
 export async function getServerSideProps(context) {
 
-    const id = context.params.id
-    
-    const resultado = await apiDeputados.get('/deputados/'+ id )
-    const deputados = resultado.data.dados
+  const id = context.params.id
 
-    const resDespesas = await apiDeputados.get('/deputados/'+ id + '/despesas' )
-    const despesas = resDespesas.data.dados
-    
-    const resProfissoes = await apiDeputados.get('/deputados/'+ id + '/profissoes' )
-    const profissoes = resProfissoes.data.dados
-    
-    return {
-      props: {deputados, despesas, profissoes }
-    }
+  const resultado = await apiDeputados.get('/deputados/' + id)
+  const deputados = resultado.data.dados
+
+  const resDespesas = await apiDeputados.get('/deputados/' + id + '/despesas')
+  const despesas = resDespesas.data.dados
+
+  const resProfissoes = await apiDeputados.get('/deputados/' + id + '/profissoes')
+  const profissoes = resProfissoes.data.dados
+
+  return {
+    props: { deputados, despesas, profissoes }
+  }
 }    
